3eebef440d858f64edd2cdb4b12934fb111bbba1
Your Name <you@example.com>
2021-12-27 11:11:24 +0700

    y

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -17,6 +17,7 @@
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css"> --}}
     
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
 </head>
 <body>
