7d44c04bb5a11648ecbb224a50de2e72a75f6cd5
Your Name <you@example.com>
2021-12-31 14:26:48 +0700

    y

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,6 +10,8 @@
     <link rel="icon" type="image/png" sizes="16x16" href="{{ secure_asset('style/images/favicon.png') }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link  rel="stylesheet" href="{{ secure_asset('style/css/style.css') }}">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    
 </head>
 
 <body class="h-100">
