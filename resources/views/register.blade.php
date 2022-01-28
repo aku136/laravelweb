5e20665f1f33abc15f127648c8ff7e5b1efc46fd
Your Name <you@example.com>
2022-01-28 20:41:54 +0700

    y

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Login Measuring BTS</title>
+    <title>Register Akun</title>
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ secure_asset('style/images/favicon.png') }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
@@ -15,6 +15,21 @@
 </head>
 
 <body class="h-100">
+    
+    <!--*******************
+        Preloader start
+    ********************-->
+    <div id="preloader">
+        <div class="loader">
+            <svg class="circular" viewBox="25 25 50 50">
+                <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10" />
+            </svg>
+        </div>
+    </div>
+    <!--*******************
+        Preloader end
+    ********************-->
+
     <div class="login-form-bg h-100">
         <div class="container h-100">
             <div class="row justify-content-center h-100">
@@ -23,27 +38,21 @@
                         <div class="card login-form mb-0">
                             <div class="card-body pt-5">
                                 <center><img src="{{ secure_asset('style/images/favicon.png') }}" height="150" width="150" alt=""></center><br>
-                                @if(session('error'))
-                                <div class="alert alert-danger">
-                                    <b>Opps!</b>{{session('error')}}
-                                </div>
-                                @endif
-                                <form class="mt-5 mb-5 login-input" action="" method="POST">
-                                    @csrf
-
+                                <form class="mt-5 mb-5 login-input">
                                     <div class="form-group">
-                                        <input type="email" class="form-control" placeholder="Email" name="email" required>
+                                        <input type="text" class="form-control"  placeholder="Name" required>
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control" placeholder="Password" name="password" required>
+                                        <input type="email" class="form-control"  placeholder="Email" required>
                                     </div>
-
-                                    <button class="btn login-form__btn submit w-100">Login</button>
-                                    {{-- <a href="{{url('home')}}" class="btn mb-1 btn-primary">
-                                        Login<span class="btn-icon-right"><i class="fa fa-sign-in" aria-hidden="true"></i></span>
-                                    </a> --}}
+                                    <div class="form-group">
+                                        <input type="password" class="form-control" placeholder="Password" required>
+                                    </div>
+                                    <button class="btn login-form__btn submit w-100">Register</button>
                                 </form>
-                                <p class="mt-5 login-form__footer">Dont have account? <a href="{{url('register')}}" class="text-primary">Sign Up</a> now</p>
+                                    <p class="mt-5 login-form__footer">Have account <a href="{{url("/")}}" class="text-primary">Login </a> now</p>
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -52,7 +61,6 @@
         </div>
     </div>
     
-
     <!--**********************************
         Scripts
     ***********************************-->
@@ -63,3 +71,8 @@
     <script src="{{secure_asset('style/js/styleSwitcher.js')}}"></script>
 </body>
 </html>
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,9 @@ use App\Http\Controllers\HomeController;
 Route::get('/', function () {
     return view('login');
 });
+Route::get('register', function () {
+    return view('register');
+});
 
 Route::get('home', function () {
     return view('home');
